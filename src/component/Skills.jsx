1db7e35ea03ec6c20debd7af52a4627673055acd
debanjo31 <debanjo31@gmail.com>
2023-02-16 09:06:59 +0100

    more animation

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -11,14 +11,20 @@ import HtmlImg from  './images/html.png'
 import BootstrapImg from  './images/bootstrap.png'
 import SassImg from  './images/sass.png'
 import FirebaseImg from  './images/firebase.png'
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
 
 
 function Skills() {
+  const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+const FadeUp = batch(Fade(), Move(), Sticky());
   return (
-    <div id='skills' className='pt-24 bg-black px-4 py-6  md:px-8 md:py-12 lg:px-12 lg:py-12 pb-96 md:pb-96 lg:pb-96 rounded-2xl'>
+    <div id='skills' className='pt-24 bg-black px-4 py-6 relative md:px-8 md:py-12 lg:px-12 lg:py-12 pb-96 md:pb-96 lg:pb-96 rounded-2xl'>
       <div className='w-5/6 mx-auto'>
       <p className='text-gray-300 text-sm lg:text-md '>ABOUT ME</p>
       <h2 className='font-bold text-3xl mt-2 lg:text-4xl lg:mb-8 lg:mt-4'>My Skills</h2>
+      <ScrollContainer>
+      <ScrollPage page={2}>
+    <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
       <div className='flex flex-wrap justify-between mb-2 px-2 py-2 gap-6'>
           <img className='block w-24 lg:w-32' alt='Git' src={Git} />
           <img className='block w-24 lg:w-32' alt='Javascript' src={JavaScript} />
@@ -32,8 +38,10 @@ function Skills() {
           <img className='block w-24 lg:w-32' alt='Bootstrap' src={BootstrapImg} />
           <img className='block w-24 lg:w-32' alt='SASS' src={SassImg} />
           <img className='block w-24 lg:w-32' alt='Firebase' src={FirebaseImg} />
-
       </div>
+      </Animator>
+      </ScrollPage>
+      </ScrollContainer>
     </div>
     </div>
   )
