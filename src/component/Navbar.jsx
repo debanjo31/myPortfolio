87a70fae96a7ceb3ad76086280108c6e60ba2ab7
debanjo31 <debanjo31@gmail.com>
2023-04-28 21:52:50 +0100

    Navrbar link now working

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,49 +1,59 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import SocialIcons from './SocialIcons';
-import { FaBars } from 'react-icons/fa';
-import Sidebar from './Sidebar';
-import { TypeAnimation } from 'react-type-animation';
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import SocialIcons from "./SocialIcons";
+import { FaBars } from "react-icons/fa";
+import Sidebar from "./Sidebar";
+import { TypeAnimation } from "react-type-animation";
 
 function Navbar() {
   const [openBar, setOpenBar] = useState(false);
   return (
     <nav className="bg-[#2a2b2f]  text-white flex items-center justify-between py-3 w-5/6 mx-auto ">
-      <div >
-      <TypeAnimation 
-      sequence={[
-        '<Adebanjo Israel />', // Types 'One'
-        1000, // Waits 1s
-       
-        () => {
-          console.log('Done typing!'); // Place optional callbacks anywhere in the array
-        }
-      ]}
-      wrapper="div"
-      speed={1}
-      cursor={false}
-      className='text-xl text-[#ffffff] font-bold lg:text-2xl '
-    />
-        
+      <div>
+        <TypeAnimation
+          sequence={[
+            "<Adebanjo Israel />", // Types 'One'
+            1000, // Waits 1s
+
+            () => {
+              console.log("Done typing!"); // Place optional callbacks anywhere in the array
+            },
+          ]}
+          wrapper="div"
+          speed={1}
+          cursor={false}
+          className="text-xl text-[#ffffff] font-bold lg:text-2xl "
+        />
       </div>
-      <div className='md:hidden'>
-                <FaBars onClick={() => setOpenBar(true)} className='text-xl cursor-pointer  mr-5'/>
-            </div>  
+      <div className="md:hidden">
+        <FaBars
+          onClick={() => setOpenBar(true)}
+          className="text-xl cursor-pointer  mr-5"
+        />
+      </div>
       <div className="hidden md:block">
         <ul className="flex text-lg gap-2 lg:text-lg lg:gap-4 ">
-          <li className=" py-2 rounded-md  font-medium leading-5 focus:outline-none focus:text-white focus:bg-gray-700 text-[#ee5e2d]">Home</li>
-          <li className=" py-2 rounded-md md:hidden lg:block font-medium leading-5 text-white  cursor-pointer focus:outline-none hover:text-[#ee5e2d]"><Link to="#about">About Me</Link></li>
-          <li className=" py-2 rounded-md  font-medium leading-5 text-white  cursor-pointer focus:outline-none hover:text-[#ee5e2d]"><Link to="#skills">My Skills</Link></li>
-          <li className=" py-2 rounded-md  font-medium leading-5 text-white  cursor-pointer focus:outline-none hover:text-[#ee5e2d]"><Link to="#works">My Works</Link></li>
-          <li className=" py-2 rounded-md md:mr-5 lg:mr-0 font-medium leading-5 text-white  cursor-pointer focus:outline-none hover:text-[#ee5e2d]"><Link to="#contact">Contact</Link></li>
-          <SocialIcons />     
+          <li className=" py-2 rounded-md  font-medium leading-5 focus:outline-none focus:text-white focus:bg-gray-700 text-[#ee5e2d]">
+            Home
+          </li>
+          <li className=" py-2 rounded-md md:hidden lg:block font-medium leading-5 text-white  cursor-pointer focus:outline-none hover:text-[#ee5e2d]">
+            <a href="#about">About Me</a>
+          </li>
+          <li className=" py-2 rounded-md  font-medium leading-5 text-white  cursor-pointer focus:outline-none hover:text-[#ee5e2d]">
+            <a href="#works">My Skills</a>
+          </li>
+          <li className=" py-2 rounded-md  font-medium leading-5 text-white  cursor-pointer focus:outline-none hover:text-[#ee5e2d]">
+            <a href="#skills">My Works</a>
+          </li>
+          <li className=" py-2 rounded-md md:mr-5 lg:mr-0 font-medium leading-5 text-white  cursor-pointer focus:outline-none hover:text-[#ee5e2d]">
+            <a href="#contact">Contact</a>
+          </li>
+          <SocialIcons />
         </ul>
       </div>
-      {openBar&& <Sidebar setOpenBar={setOpenBar} />}
+      {openBar && <Sidebar setOpenBar={setOpenBar} />}
     </nav>
-    );
+  );
 }
 
 export default Navbar;
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,26 +1,54 @@
-import React from 'react'
-import SocialIcons from './SocialIcons';
+import React from "react";
+import SocialIcons from "./SocialIcons";
 
-
-function Sidebar({setOpenBar}) {
+function Sidebar({ setOpenBar }) {
   return (
-    <div className='min-h-100 sidebar z-60 overflow-hidden	snap-none  bg-[#141414] absolute text-white inset-0'>
-        <div className='flex justify-between mx-4 my-4 '>
-            <img src="" className="block" alt='' />
-            <p onClick={()=> setOpenBar(false)} className='block font-bold cursor-pointer text-2xl mt-3 mr-5'>X</p>
+    <div className="min-h-100 sidebar z-60 overflow-hidden	snap-none  bg-[#141414] absolute text-white inset-0">
+      <div className="flex justify-between mx-4 my-4 ">
+        <img src="" className="block" alt="" />
+        <p
+          onClick={() => setOpenBar(false)}
+          className="block font-bold cursor-pointer text-2xl mt-3 mr-5"
+        >
+          X
+        </p>
+      </div>
+      <div
+        style={{ height: "80vh", justifyContent: "space-evenly" }}
+        className="min-h-80 font-bold text-xl gap-12 flex-col items-center flex  justify-items-col-evenly space-around"
+      >
+        <p>
+          <a href="#home" onClick={() => setOpenBar(false)}>
+            Home
+          </a>
+        </p>
+        <p>
+          <a href="#about" onClick={() => setOpenBar(false)}>
+            About Me
+          </a>
+        </p>
+        <p>
+          <a href="#works" onClick={() => setOpenBar(false)}>
+            My Skills
+          </a>
+        </p>
+        <p>
+          {" "}
+          <a href="#skills" onClick={() => setOpenBar(false)}>
+            My Works
+          </a>
+        </p>
+        <p>
+          <a href="#contact" onClick={() => setOpenBar(false)}>
+            Contact
+          </a>
+        </p>
+        <div className="text-3xl">
+          <SocialIcons />
         </div>
-        <div style={{height : "80vh", justifyContent : "space-evenly"}} className='min-h-80 font-bold text-xl gap-12 flex-col items-center flex  justify-items-col-evenly space-around'>
-            <p><a href="#home" onClick={()=> setOpenBar(false)} >Home</a></p>
-            <p><a href="#about" onClick={()=> setOpenBar(false)}>About Me</a></p>
-            <p><a href="#works" onClick={()=> setOpenBar(false)}>My Skills</a></p>
-            <p> <a href="#skills" onClick={()=> setOpenBar(false)}>My Works</a></p>
-            <p><a href="#contact" onClick={()=> setOpenBar(false)}>Contact</a></p>
-            <div className='text-3xl'>
-                <SocialIcons /> 
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
